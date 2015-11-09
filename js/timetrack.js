c531c9320020082f0e62f76630633c022e5d013e
Stefan Lange-Hegermann <stefan@app-architect.com>
2015-11-10 00:56:22 +0100

    Initial version of timetrack express (Wann ist Feierabend?)

---
+++ js/timetrack.js
@@ -0,0 +1,50 @@
+function pad(num) {
+    if ((num+"").length <2) {
+        return "0"+num;
+    }
+    return num;
+}
+$(document).ready(function() {
+    var tock = true;
+    var now = new Date();
+    $("#hour_in").val(now.getHours());
+    $("#minute_in").val(parseInt(now.getMinutes()));
+    Piecon.setOptions({
+        color: '#848788', // Pie chart color
+        background: '#ffffff', // Empty pie chart color
+        shadow: '#479263', // Outer ring color
+        fallback: false // Toggles displaying percentage in the title bar (possible values - true, false, 'force')
+    });
+    var update = function() {
+        var now = new Date();
+        var in_time = new Date();
+        in_time.setHours($("#hour_in").val());
+        in_time.setMinutes($("#minute_in").val());
+        in_time.setSeconds(0);
+
+        var required_time = new Date((parseInt($("#hours_required").val())*60+parseInt($("#minutes_required").val()))*60000);
+        var elapsed = (now - in_time);
+        var diff = new Date(elapsed);
+        var end_time = new Date(in_time.getTime() + required_time.getTime());
+        var remaining_time = new Date(end_time-now);
+
+        var percent_done=(required_time-remaining_time)/required_time*100;
+        console.log(percent_done);
+        $("#time_progress").css("width", percent_done+"%");
+        $("#in_time").html(pad(diff.getUTCHours())+" Stunden<br>"+pad(diff.getUTCMinutes())+" Minuten<br>"+pad(diff.getUTCSeconds())+ " Sekunden");
+        $("#finishing_at").text(pad(end_time.getHours())+":"+pad(end_time.getMinutes()));
+        $("#finishing_in").html("in&nbsp;"+pad(remaining_time.getUTCHours())+"&nbsp;Stunden "+pad(remaining_time.getUTCMinutes())+"&nbsp;Minuten ");
+        $("title").text(pad(remaining_time.getUTCHours())+":"+pad(remaining_time.getUTCMinutes())+" bis Feierabend");
+
+        Piecon.setOptions({
+            color: '#848788', // Pie chart color
+            background: (tock ? '#ffffff' : "#eeeeee"), // Empty pie chart color
+            shadow: '#479263', // Outer ring color
+            fallback: false
+        });
+        tock=!tock;
+        Piecon.setProgress(percent_done);
+    };
+    update();
+    setInterval(update, 1000);
+})
