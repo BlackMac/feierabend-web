612f7d3fc6628ce66f6cbb94c740be4642bf7cd3
Stefan Lange-Hegermann <stefan@app-architect.com>
2015-11-12 01:08:35 +0100

    new style is more focused on time

--- js/timetrack.js
+++ js/timetrack.js
@@ -41,62 +41,80 @@ function requiredMinutes(minutes) {
   return parseInt(requiredMinutes)
 }
 $(document).ready(function() {
-    var tock = true;
+  /*$(document).scroll(function() {
+    if ($(this).scrollTop()>250) {
+      $(".container").removeClass("full");
+    }
+  });*/
 
-    $("#hour_in").val(startHours());
-    $("#minute_in").val(startMinutes());
-    $("#hours_required").val(requiredHours);
-    $("#minutes_required").val(requiredMinutes);
+  $(".handle").click(function() { $(".control").toggleClass("preview")});
+  $("input:text").focus(function() { $(this).select(); } );
+  $("input:text").keyup(function () {
+    if (this.value.length == this.maxLength) {
+      $(this).nextAll('input:text').focus();
+    }
+    update();
+  });
+  window.setTimeout(function() {
+      $("div.control").removeClass("preview")
+    }, 1500);
+
+  var tock = true;
+
+  $("#hour_in").val(startHours());
+  $("#minute_in").val(startMinutes());
+  $("#hours_required").val(requiredHours);
+  $("#minutes_required").val(requiredMinutes);
+
+  Piecon.setOptions({
+      color: '#0f2231', // Pie chart color
+      background: '#ffffff', // Empty pie chart color
+      shadow: '#000000', // Outer ring color
+      fallback: false // Toggles displaying percentage in the title bar (possible values - true, false, 'force')
+  });
+  var update = function() {
+    startHours($("#hour_in").val());
+    startMinutes($("#minute_in").val());
+    requiredHours($("#hours_required").val());
+    requiredMinutes($("#minutes_required").val());
+
+    var now = new Date();
+    var in_time = new Date();
+    in_time.setHours($("#hour_in").val());
+    in_time.setMinutes($("#minute_in").val());
+    in_time.setSeconds(0);
+
+    var required_time = new Date((parseInt($("#hours_required").val())*60+parseInt($("#minutes_required").val()))*60000);
+    var elapsed = (now - in_time);
+    var diff = new Date(elapsed);
+    var end_time = new Date(in_time.getTime() + required_time.getTime());
+    var remaining_time = new Date(end_time-now);
+
+    var percent_done=(required_time-remaining_time)/required_time*100;
+
+    $("#in_time").html(pad(diff.getUTCHours())+" Stunden<br>"+pad(diff.getUTCMinutes())+" Minuten<br>"+pad(diff.getUTCSeconds())+ " Sekunden");
+    $("#finishing_at").text(pad(end_time.getHours())+":"+pad(end_time.getMinutes()));
+    if (Math.round(percent_done) < 100) {
+      $("#time_progress").css("width", percent_done+"%");
+      $("#finishing_in").html("in&nbsp;"+pad(remaining_time.getUTCHours())+"&nbsp;Stunden "+pad(remaining_time.getUTCMinutes())+"&nbsp;Minuten ");
+      $("title").text(pad(remaining_time.getUTCHours())+":"+pad(remaining_time.getUTCMinutes())+" bis Feierabend");
+    } else {
+      percent_done = 100;
+      $("#time_progress").css("width", "100%");
+      remaining_time = new Date(now-end_time);
+      $("#finishing_in").html("vor&nbsp;"+pad(remaining_time.getUTCHours())+"&nbsp;Stunden "+pad(remaining_time.getUTCMinutes())+"&nbsp;Minuten ");
+      $("title").text(pad(remaining_time.getUTCHours())+":"+pad(remaining_time.getUTCMinutes())+" seit Feierabend");
+    }
 
     Piecon.setOptions({
         color: '#0f2231', // Pie chart color
-        background: '#ffffff', // Empty pie chart color
+        background: (tock ? '#ffffff' : "#eeeeee"), // Empty pie chart color
         shadow: '#000000', // Outer ring color
-        fallback: false // Toggles displaying percentage in the title bar (possible values - true, false, 'force')
+        fallback: false
     });
-    var update = function() {
-      startHours($("#hour_in").val());
-      startMinutes($("#minute_in").val());
-      requiredHours($("#hours_required").val());
-      requiredMinutes($("#minutes_required").val());
-
-      var now = new Date();
-      var in_time = new Date();
-      in_time.setHours($("#hour_in").val());
-      in_time.setMinutes($("#minute_in").val());
-      in_time.setSeconds(0);
-
-      var required_time = new Date((parseInt($("#hours_required").val())*60+parseInt($("#minutes_required").val()))*60000);
-      var elapsed = (now - in_time);
-      var diff = new Date(elapsed);
-      var end_time = new Date(in_time.getTime() + required_time.getTime());
-      var remaining_time = new Date(end_time-now);
-
-      var percent_done=(required_time-remaining_time)/required_time*100;
-
-      $("#in_time").html(pad(diff.getUTCHours())+" Stunden<br>"+pad(diff.getUTCMinutes())+" Minuten<br>"+pad(diff.getUTCSeconds())+ " Sekunden");
-      $("#finishing_at").text(pad(end_time.getHours())+":"+pad(end_time.getMinutes()));
-      if (Math.round(percent_done) < 100) {
-        $("#time_progress").css("width", percent_done+"%");
-        $("#finishing_in").html("in&nbsp;"+pad(remaining_time.getUTCHours())+"&nbsp;Stunden "+pad(remaining_time.getUTCMinutes())+"&nbsp;Minuten ");
-        $("title").text(pad(remaining_time.getUTCHours())+":"+pad(remaining_time.getUTCMinutes())+" bis Feierabend");
-      } else {
-        percent_done = 100;
-        $("#time_progress").css("width", "100%");
-        remaining_time = new Date(now-end_time);
-        $("#finishing_in").html("vor&nbsp;"+pad(remaining_time.getUTCHours())+"&nbsp;Stunden "+pad(remaining_time.getUTCMinutes())+"&nbsp;Minuten ");
-        $("title").text(pad(remaining_time.getUTCHours())+":"+pad(remaining_time.getUTCMinutes())+" seit Feierabend");
-      }
-
-      Piecon.setOptions({
-          color: '#0f2231', // Pie chart color
-          background: (tock ? '#ffffff' : "#eeeeee"), // Empty pie chart color
-          shadow: '#000000', // Outer ring color
-          fallback: false
-      });
-      tock=!tock;
-      Piecon.setProgress(percent_done);
-    };
-    update();
-    setInterval(update, 1000);
+    tock=!tock;
+    Piecon.setProgress(percent_done);
+  };
+  update();
+  setInterval(update, 1000);
 })
