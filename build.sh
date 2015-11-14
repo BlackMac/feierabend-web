c2b2bdec7a08dbcc30533e5d0de82f21d2732111
Stefan Lange-Hegermann <stefan@app-architect.com>
2015-11-14 02:23:12 +0100

    Create a build directory when built

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
 #! /bin/sh
-uglifyjs --compress --mangle -o js/tt.js -- js/lib/*.js js/timetrack/*.js js/timetrack.js
+rm -rf build
+mkdir build
+mkdir build/js
+uglifyjs --compress --mangle -o build/js/tt.js -- js/lib/*.js js/timetrack/*.js js/timetrack.js
+
+cp -r css build/css
+cp -r bg build/bg
+cp datenschutz.html build/
+cp impressum.html build/
+cp favicon.ico build/
+
+sed 's/<!-- JS -->/<script src="js\/tt.js"><\/script> <!--/g' index.html > build/index.html
